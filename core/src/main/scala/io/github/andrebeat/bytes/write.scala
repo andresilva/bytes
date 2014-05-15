4c8f373a3061d2fb957016f9f7de8bd5461729e2
Andre Silva <andre.beat@gmail.com>
2014-05-16 00:13:22 +0100

    Write: return number of bytes written

--- core/src/main/scala/io/github/andrebeat/bytes/write.scala
+++ core/src/main/scala/io/github/andrebeat/bytes/write.scala
@@ -1,35 +1,56 @@
 package io.github.andrebeat.bytes
 
 trait Write[A] {
-  def apply(bytes: Bytes, offset: Int, value: A): Unit
+  def apply(bytes: Bytes, offset: Int, value: A): Int
 }
 
 object Write {
   implicit object ByteWrite extends Write[Byte] {
-    def apply(bytes: Bytes, offset: Int, value: Byte) = bytes.writeByte(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Byte): Int = {
+      bytes.writeByte(offset, value)
+      1
+    }
   }
 
   implicit object ShortWrite extends Write[Short] {
-    def apply(bytes: Bytes, offset: Int, value: Short) = bytes.writeShort(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Short) = {
+      bytes.writeShort(offset, value)
+      2
+    }
   }
 
   implicit object CharWrite extends Write[Char] {
-    def apply(bytes: Bytes, offset: Int, value: Char) = bytes.writeChar(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Char) = {
+      bytes.writeChar(offset, value)
+      2
+    }
   }
 
   implicit object IntWrite extends Write[Int] {
-    def apply(bytes: Bytes, offset: Int, value: Int) = bytes.writeInt(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Int) = {
+      bytes.writeInt(offset, value)
+      4
+    }
   }
 
   implicit object FloatWrite extends Write[Float] {
-    def apply(bytes: Bytes, offset: Int, value: Float) = bytes.writeFloat(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Float) = {
+      bytes.writeFloat(offset, value)
+      4
+    }
   }
 
   implicit object LongWrite extends Write[Long] {
-    def apply(bytes: Bytes, offset: Int, value: Long) = bytes.writeLong(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Long) = {
+      bytes.writeLong(offset, value)
+      8
+    }
   }
 
   implicit object DoubleWrite extends Write[Double] {
-    def apply(bytes: Bytes, offset: Int, value: Double) = bytes.writeDouble(offset, value)
+    def apply(bytes: Bytes, offset: Int, value: Double) = {
+      bytes.writeDouble(offset, value)
+      8
+    }
   }
 }
